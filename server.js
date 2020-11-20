c8012c95982fcaad1d010a65af35196127762360
Kurt Ball <kurtisdball@gmail.com>
2020-11-19 21:34:30 -0700

    Server and function.js updated

--- server.js
+++ server.js
@@ -1,52 +1,49 @@
-let bottles =[ 
+let knownBottles = [
   {
-    "id": "1",
-    "upc": 0067000104022,
-    "brand": "Coca Cola",
-    "volume": "755 mL"
-  }, 
+    id: '1',
+    upc: '0067000104022',
+    brand: 'Coca Cola',
+    volume: '755 mL',
+    value: '0.1',
+  },
   {
-    "id": "2",
-    "upc": 06741602,
-    "brand": "Coca Cola",
-    "volume": "1 Liter"
-  }
+    id: '2',
+    upc: '06741602',
+    brand: 'Coca Cola',
+    volume: '1 Liter',
+    value: '0.25',
+  },
 ]
+bottles = []
 
 const express = require('express')
-const cors = require('cors')    
+const cors = require('cors')
 const app = express()
+//const request = require('request')
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('.'))
-//app.use(bodyParser.json());
+//app.use(express.static('.'))
 
-let server = app.listen(5000, function(){
-  var port = server.address().port
-  console.log(`The server is up on ${port}`)
-})
+//PORTS
+//Don't forget to run export PORT=5000 in the terminal
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server listening on ${port}...`))
 
-app.get('/bottles', function(req, res){
+app.get('/bottles', (req, res) => {
   res.send(bottles)
 })
 
+app.post('/bottles', function (req, res) {
+  const scannedItem = req.body
 
+  console.log(`This should be a number ${scannedItem.bottleUPC}`)
 
-app.post('/bottles', function(req, res){
-  const newRecord = req.body
-  console.log(typeof(newRecord.bottleUPC))
-  newRecord = newRecord.parseInt(newRecord.bottleUPC)
-  console.log(`This should be a number ${newRecord}`)
-
-  bottles.forEach(bottle => {
-    console.log(typeof(bottle.upc))
-    if(newRecord.bottleUPC === bottle.upc){
-      
-      newRecord.id = bottles.length+1
-      bottles.push(req.body)
-
-    }  else{
+  knownBottles.forEach((bottle) => {
+    if (scannedItem.bottleUPC === bottle.upc) {
+      scannedItem.id = bottles.length + 1
+      bottles.push(bottle)
+    } else {
       console.log('Please scan a valid code')
     }
     // console.log(`This is a test ${bottle.upc}`)
