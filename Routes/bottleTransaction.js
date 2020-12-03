b4a9abec660d75d2e54c7b579b1077755dc0c929
Shaun-Derrick <derrickshaun@gmail.com>
2020-12-02 17:57:50 -0700

    Completed process flow to receipt

--- Routes/bottleTransaction.js
+++ Routes/bottleTransaction.js
@@ -1,67 +1,95 @@
-const router = require('express').Router()
+const router = require("express").Router();
 //SMALL API FOR KNOWN BOTTLE TYPES
 let knownBottles = [
   {
     id: 1,
-    upc: '0067000104022',
-    brand: 'Coca Cola',
+    upc: "0067000104022",
+    brand: "Coca Cola",
     volume: 0.755,
-    value: '0.1',
+    value: "0.1",
   },
   {
     id: 2,
-    upc: '06741602',
-    brand: 'Coca Cola',
+    upc: "06741602",
+    brand: "Coca Cola",
     volume: 1,
-    value: '0.25',
+    value: "0.25",
   },
-]
+  {
+    id: 3,
+    upc: "048415345101",
+    brand: "Royal Reserve",
+    volume: 0.75,
+    value: "0.10",
+  },
+  {
+    id: 4,
+    upc: "025293600713",
+    brand: "Silk",
+    volume: 2,
+    value: "0.25",
+  },
+];
+// Added to other unique bottles to known bottles
 
 let bottleTransaction = {
   id: 0,
+  terminal: 1001,
+  transactionNumber: 10001,
   date: Date(),
   total: 0,
   value: 0,
   over1L: 0,
   under1L: 0,
   bottleList: [],
-}
+  processed: false,
+};
+// added keys and values to bottleTransaction
 
 //RETURNS THE BOTTLE TRANSACTION TO THE USER ON THE FRONTEND
-router.get('/bottles', (req, res) => {
-  res.send(bottleTransaction)
-})
+router.get("/bottles", (req, res) => {
+  res.send(bottleTransaction);
+});
 //WHERE THE BOTTLES PER TRANSACTION IS CALCULATED
-router.post('/bottles', function (req, res) {
-  const scannedItem = req.body
+router.post("/bottles", function (req, res) {
+  const scannedItem = req.body;
 
-  console.log(`This should be a number ${scannedItem.upc}`)
+  console.log(`This should be a number ${scannedItem.upc}`);
 
   knownBottles.forEach((bottle) => {
     if (scannedItem.upc === bottle.upc) {
       //bottleTransaction.id = bottleTransaction.length + 1
 
       //Bottle Count
-      bottleTransaction.total = bottleTransaction.total += 1
+      bottleTransaction.total = bottleTransaction.total += 1;
 
       //Sumtotal and litre size count
       if (bottle.volume >= 1) {
-        bottleTransaction.value += 0.25
-        bottleTransaction.over1L += 1
+        bottleTransaction.value += 0.25;
+        bottleTransaction.over1L += 1;
       } else if (bottle.volume < 1) {
-        bottleTransaction.value += 0.1
-        bottleTransaction.under1L += 1
+        bottleTransaction.value += 0.1;
+        bottleTransaction.under1L += 1;
       } else {
-        return 0
+        return 0;
       }
 
-      bottleTransaction.bottleList.push(bottle)
+      bottleTransaction.bottleList.push(bottle);
     } else {
-      console.log('This item does not match')
+      console.log("This item does not match");
     }
     // console.log(`This is a test ${bottle.upc}`)
-  })
-  console.log(bottleTransaction)
-})
+  });
+  console.log(bottleTransaction);
+});
+// added new post response to change processed key value from false to true
+app.post("/api/bottles/bottles2", (req, res) => {
+  const changes = req.body;
+  console.log(`Changes is : ${JSON.stringify(changes)}`);
+  bottleTransaction = { ...bottleTransaction, ...changes };
+  console.log(`Bottle transaction is : ${JSON.stringify(bottleTransaction)}`);
+  // Object.assign(changes);
+  res.send(bottleTransaction);
+});
 
-module.exports = router
+module.exports = router;
--- functions.js
+++ functions.js
@@ -1,24 +1,42 @@
 function allBottles() {
-  fetch('http://localhost:5000/api/bottles/bottles')
+  fetch("http://localhost:5000/api/bottles/bottles")
     .then((response) => response.json())
     .then((bottles) => {
       let orderTable = `
       <ul>
           <li>$${bottles.value.toFixed(2)}</li>
-        </ul>`
-      document.getElementById('orderTable').innerHTML = orderTable
-    })
+        </ul>`;
+      document.getElementById("orderTable").innerHTML = orderTable;
+    });
 }
 
 function scanBottle() {
-  const bottleUpc = document.getElementById('numberSubmit')
+  const bottleUpc = document.getElementById("numberSubmit");
 
-  const upcCode = bottleUpc.value
-  fetch('http://localhost:5000/api/bottles/bottles', {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
+  const upcCode = bottleUpc.value;
+  fetch("http://localhost:5000/api/bottles/bottles", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ upc: upcCode }),
-  }).then(allBottles())
+  }).then(allBottles());
 }
 
-console.log(allBottles())
+console.log(allBottles());
+// added function to change processed key value to true
+function processTransaction() {
+  fetch("http://localhost:5000/api/bottles/bottles2", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ processed: true }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      window.location = "refund.html";
+    });
+}
+// Added reward function to receipt page
+function reward() {
+  alert(
+    "Your business is appreciated! We would like to show our apreciation to you by providing a coupon for $2.00 off your next coffee at our partner Good Earth Cafe.  The coupon is on the back of your receipt."
+  );
+}
